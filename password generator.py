ffbcbc4b61d610feccf269312970350b7c75f303
Abhishek <88420201+abhiijndal@users.noreply.github.com>
2021-08-27 19:34:11 +0530

    Add files via upload

---
+++ password generator.py
@@ -0,0 +1,21 @@
+import string 
+import random
+if __name__=="__main__":
+    s1= string.ascii_lowercase
+    s2= string.ascii_uppercase
+    s3=string.punctuation
+    s4=string.digits
+    
+    
+    plen = int(input("enter lenght of password\n"))
+
+    s = []
+    s.extend(list(s1))
+    s.extend(list(s2))
+    s.extend(list(s3))
+    s.extend(list(s4))
+
+    random.shuffle(s)
+     
+    print("your password is:")
+    print("".join(s[0:plen]))
